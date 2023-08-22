883608f36fcfed32e5cb48af0dce43dcc1ce75c4
Rahulk1k <118955422+Rahulk1k@users.noreply.github.com>
2023-08-22 19:33:28 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,1 +1,13 @@
 //your code here
+ .block-inline-button,
+  .block-inline-div,
+  .block-inline-span,
+  .block-inline-p,
+  .block-inline-input,
+  .block-inline-cite,
+  .block-inline-code,
+  .block-inline-h1,
+  .block-inline-sup,
+  .block-inline-sub {
+    display: inline; /* You can use 'block' or 'inline-block' based on the element */
+  }
